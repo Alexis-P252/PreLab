334b5e5e78010b3a2fd664af40b72ca052e6cbc2
Alexis-P252 <88517376+Alexis-P252@users.noreply.github.com>
2022-04-18 21:58:33 -0300

    creacion navbar

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css', '../../styles.css']
+
 })
 export class HomePageComponent implements OnInit {
 
